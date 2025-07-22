11db6ad70b7fd82cb08e8cc89f716920149ec853
nero <237326369@qq.com>
2025-07-22 09:30:52 +0000

    chore: 更新版本号至2.0.10

--- Movie_Data_Capture.py
+++ Movie_Data_Capture.py
@@ -745,7 +745,7 @@ def period(delta, pattern):
 
 
 if __name__ == '__main__':
-    version = '2.0.5'
+    version = '2.0.10'
     urllib3.disable_warnings()  # Ignore http proxy warning
     app_start = time.time()
 
